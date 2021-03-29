a642c54e3ccfe980505059db9379eb4b76fac7ae
sangeet <sangeet.khatri@protonmail.com>
2021-03-29 12:51:00 +0530

    Fixed broken config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
